4606fab4e0ddd22818c6741913c834cbd8c4ef2a
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-07-31 21:49:22 -0400

    trying different data forms

--- index.php
+++ index.php
@@ -5,9 +5,19 @@
 
 // echo json_encode(42);
 // die();
+//header('Content-Type: application/json');
+//header('Content-Type: html/text');
+// echo "got it";
+// $test = "hello";
+// echo json_encode($test);
+// die();
+// header('Content-Type: multipart/form-data');
+
 $received = file_get_contents('php://input');
-$received = json_decode($received);
-$received = json_encode($received);
+var_dump($received);
+die();
+//$received = json_decode($received);
+//$received = json_encode($received);
 echo $received;
 die();
 //header('Content-Type: application/json');
@@ -19,6 +29,5 @@ echo "hello" + $converted;
 die();
 //print_r($converted);
 //var_dump($) 
-header('Content-Type: application/json');
 echo json_encode($converted, true);
 
